8187b5f346b2ddb7ec8132d67a0588c40ad7c394
Paulo Ricardo <pauloricardo.st1812@hotmail.com>
2025-09-26 11:17:14 -0300

    Inserir dados na  tabela

--- main.py
+++ main.py
@@ -9,12 +9,43 @@ cursor = conexao.cursor()
 
 
 
-#criar uma tabela mo banco de dados
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT
-    )
-""")
+# #criar uma tabela mo banco de dados
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT
+#     )
+# """)
+# nome = input("Digite o nome do aluno: ").lower()
+# idade = int(input("Digite a idade do aluno: "))
+# curso = input("Digite o curso do aluno:").lower()
+
+# #Inserir um dado na tabela
+# cursor.execute("""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?, ?, ?)
+# """,
+# (nome, idade, curso)
+# )
+
+
+# #confirmar as alterações no banco
+# conexao.commit()
+
+
+#Inserir varios alunos uma vez só
+alunos =[
+    ("Yago", 28, "Direito"),
+    ("Jessica", 24, "computação"),
+    ("Breno", 22, "Computação")
+]
+#executemany pemite inserir multiplas linhas de uma vez só
+cursor.executemany("""
+INSERT INTO alunos(nome, idade, curso)
+VALUES(?, ?, ?)
+""",
+(alunos)
+)
+conexao.commit()
