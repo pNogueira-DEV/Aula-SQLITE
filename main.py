a6a84231f9dbfaf98ef538d9e07712448ac6af7b
Paulo Ricardo <pauloricardo.st1812@hotmail.com>
2025-09-26 10:27:59 -0300

    Criar a tabela alunos

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ conexao = sqlite3.connect("escola.db")
 
 #criar o objeto chamado de "cursor" que sera usado para executar os comandos SQL
 cursor = conexao.cursor()
+
+
+
+#criar uma tabela mo banco de dados
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS alunos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER,
+    curso TEXT
+    )
+""")
